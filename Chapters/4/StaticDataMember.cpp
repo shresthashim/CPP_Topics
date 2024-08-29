4891ee66bde10cef417d4ac0cff818a6609d2e1f
AsHim Shrestha <ashimsth89@gmail.com>
2024-08-29 17:54:29 +0545

    add comments

--- Chapters/4/StaticDataMember.cpp
+++ Chapters/4/StaticDataMember.cpp
@@ -1,31 +1,34 @@
-//Program  to count the number of objects created manually(Note:the numbers mustn't provided by users)
+// Program  to count the number of objects created manually(Note:the numbers mustn't provided by users)
 #include <iostream>
 using namespace std;
 
-class ObjectCounter {
+class ObjectCounter
+{
 private:
-    static int count; 
+    static int count; // This is a static data member
 
 public:
-    ObjectCounter() {
-        count++; 
+    ObjectCounter()
+    {
+        count++;
     }
 
-   
-    static int getCount() {
+    // This is a static member function and it can only access static data members
+    static int getCount()
+    {
         return count;
     }
 };
 
+int ObjectCounter::count = 0; // This is how you initialize a static data member... we cannot initialize it inside the class
 
-int ObjectCounter::count = 0;
-
-int main() {
+int main()
+{
     ObjectCounter obj1;
     ObjectCounter obj2;
     ObjectCounter obj3;
 
-    cout << "Number of objects created: " << ObjectCounter::getCount() << endl;
+    cout << "Number of objects created: " << ObjectCounter::getCount() << endl; // This is how you invoke a static member function of a class by using classname::function_name ....we cannot use obj1.getCount() or blablabla here i.e cannot use object to access static member function
 
     return 0;
 }
