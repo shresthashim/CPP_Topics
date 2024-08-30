18c3895625ea3098f7cfe1716d515ff98c85fd59
AsHim Shrestha <ashimsth89@gmail.com>
2024-08-30 11:29:58 +0545

    refactor: Override show() function in class C to demonstrate member function overriding

---
+++ Chapters/6/MemberFunctionOverriding.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    void show()
+    {
+        cout << "Class A" << endl;
+    }
+};
+
+class B
+{
+public:
+    void show()
+    {
+        cout << "Class B" << endl;
+    }
+};
+
+class C : public A, public B
+{
+public:
+    void show()
+    {
+        cout << "Class C" << endl;
+    }
+};
+
+int main()
+{
+    C obj;
+    obj.show(); // Output: Class C ... it overrides the show() function of both base classes A and class B
+
+    return 0;
+}
