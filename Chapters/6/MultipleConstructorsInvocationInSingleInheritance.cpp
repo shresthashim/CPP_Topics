7684c22b872eb7578907929cab27568da834f2a4
AsHim Shrestha <ashimsth89@gmail.com>
2024-08-30 13:02:13 +0545

    refactor: Add constructor invocation in single inheritance example

---
+++ Chapters/6/MultipleConstructorsInvocationInSingleInheritance.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+public:
+    Base()
+    {
+        cout << "Constructor of Base class" << endl;
+    }
+    Base(int x)
+    {
+        cout << "Constructor of Base class with value " << x << endl;
+    }
+};
+
+class Derived : public Base
+{
+public:
+    Derived(int x)
+    {
+        cout << "Constructor of Derived class with value " << x << endl;
+    }
+};
+
+int main()
+{
+    Derived d(5);
+
+    return 0;
+}
+// Output:
+// Constructor of Base class
+// Constructor of Derived class with value 5
