bdbfec8acef81196c8738affc570c3c0ce143f8f
AsHim Shrestha <ashimsth89@gmail.com>
2024-08-31 11:19:22 +0545

    refactor: Improve exception handling and remove unnecessary code in HandlingUncaughtException.cpp

--- Chapters/10/HandlingUncaughtException.cpp
+++ Chapters/10/HandlingUncaughtException.cpp
@@ -1,67 +1,66 @@
-// #include<iostream>
-
-// using namespace std;
-
-// void test_handler(){
-// cout<<"INSIDE TEST HANDLER"<<endl;
-
-// }
-
-// int main (){
-//     set_terminate(test_handler);
-
-//     try
-//     {
-//         throw 10;
-//     }
-//     catch(char c)
-//     {
-//         cerr << "Caught a character: " << c << endl;
-//     }
-
-// }
-
 #include <iostream>
-#include <cmath>     // For sqrt function
-#include <stdexcept> // For standard exceptions
 
 using namespace std;
 
-double calculateSquareRoot(double number)
+void test_handler()
 {
-    if (number < 0)
-    {
-        throw invalid_argument("Cannot calculate square root of a negative number."); // Throw exception
-    }
-    return sqrt(number); // Calculate square root
+    cout << "INSIDE TEST HANDLER" << endl;
 }
-
-void terminateAlternative()
-{
-    cerr << "Uncaught exception. Program will terminate." << endl;
-}
-
 
 int main()
 {
-
-    set_terminate(terminateAlternative);
-
-
-    double num;
-    cout << "Enter a number: ";
-    cin >> num;
+    set_terminate(test_handler);
 
     try
     {
-        double result = calculateSquareRoot(num); // Call function that might throw an exception
-        cout << "Square root: " << result << endl;
+        throw 10;
     }
-    catch (const invalid_argument &e)
+    catch (char c)
     {
-        cerr << "Error: " << e.what() << endl; // Catch and display exception with argument
-        throw;
+        cerr << "Caught a character: " << c << endl;
     }
-
     return 0;
 }
+
+// #include <iostream>
+// #include <cmath>     // For sqrt function
+// #include <stdexcept> // For standard exceptions
+
+// using namespace std;
+
+// double calculateSquareRoot(double number)
+// {
+//     if (number < 0)
+//     {
+//         throw invalid_argument("Cannot calculate square root of a negative number."); // Throw exception
+//     }
+//     return sqrt(number); // Calculate square root
+// }
+
+// void terminateAlternative()
+// {
+//     cerr << "Uncaught exception. Program will terminate." << endl;
+// }
+
+// int main()
+// {
+
+//     set_terminate(terminateAlternative);
+
+//     double num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+
+//     try
+//     {
+//         double result = calculateSquareRoot(num); // Call function that might throw an exception
+//         cout << "Square root: " << result << endl;
+//     }
+//     catch (const invalid_argument &e)
+//     {
+//         cerr << "Error: " << e.what() << endl; // Catch and display exception with argument
+//         throw;
+//     }
+
+//     return 0;
+// }
